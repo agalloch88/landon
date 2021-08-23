7581f1b44279322850d44633c4a54e5a5c1efcac
Ryan Kirsch <ryan.kirsch@longevityconsulting.com>
2021-08-23 09:15:01 -0400

    add Main component, nested Welcome and HotelInfo inside to refactor code and clean up App.js

---
+++ src/components/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import HotelInfo from './HotelInfo';
+import Welcome from './Welcome';
+
+
+function Main() {
+    return (
+        <div>
+            <Welcome />
+            <HotelInfo />
+        </div>
+    )
+}
+
+export default Main;
